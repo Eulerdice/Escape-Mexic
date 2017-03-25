e1faae0deb9f388281d7471278b7db43ac1a83fc
Alex Bondrea <sabondrea1@sheffield.ac.uk>
2017-03-25 16:49:00 +0000

    Added some sprites for player

--- Escape From Mexic/Assets/Movement.cs
+++ Escape From Mexic/Assets/Movement.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 
 
 public class Movement : MonoBehaviour {
 
     public float speed = 10f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,6 +18,9 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (Input.GetKeyDown(KeyCode.UpArrow)) ;
+
+
         GetComponent<Rigidbody2D>().velocity = targetVelocity * speed;
     }
 }
